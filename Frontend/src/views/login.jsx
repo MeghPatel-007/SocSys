f65252c7583688cd8618f823ba95d9607709e400
MeghPatel-007 <meghpatel0009@gmail.com>
2026-04-08 00:21:30 +0530

    bug

--- Frontend/src/views/login.jsx
+++ Frontend/src/views/login.jsx
@@ -344,7 +344,7 @@ function Login() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/`,
+          redirectTo: `${window.location.origin}/dashboard/user`,
         },
       });
 
